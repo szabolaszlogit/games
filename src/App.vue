afa56292993b843eb3ce9c2b017e6cb65905d489
szab9laszlogit <you@example.comszabolaszlo.mail@gmail.com>
2022-11-22 22:16:09 +0100

    anim

--- src/App.vue
+++ src/App.vue
@@ -1,89 +1,31 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-import RockPaperScissors from './components/rps/RockPaperScissors.vue'
+import { ref } from "vue";
+import { RouterLink, RouterView } from "vue-router";
+import RockPaperScissors from "./components/rps/RockPaperScissors.vue";
 
+const componentKey = ref(0);
+
+const forceRerender = () => {
+  componentKey.value += 1;
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-<section>
-  <rock-paper-scissors></rock-paper-scissors>
-</section>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+  </nav>
+  <button
+    class="ma2 pointer f6 grow br-pill ph3 pv2 mb2 dib white bg-dark-green"
+    v-on:click="forceRerender()"
+  >
+    Reset
+  </button>
+  <section>
+    <rock-paper-scissors :key="componentKey"></rock-paper-scissors>
+  </section>
   <RouterView />
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
--- src/components/rps/RockPaperScissors.vue
+++ src/components/rps/RockPaperScissors.vue
@@ -1,58 +1,144 @@
 <script setup>
-import { ref, computed } from "vue";
+import { ref, reactive, computed, watch } from "vue";
 
-const availableChoices = ref([
+const availableChoices = reactive([
   { name: "Scissors", icon: "✂️" },
-  { name: "Paper", icon: "🧻 " },
+  { name: "Paper", icon: "🧻" },
   { name: "Rock", icon: "💎" },
 ]);
-let computerChoice = ref("");
-let playerChoice = ref("");
+let computerChoice = reactive({});
+let playerChoice = reactive({});
+
+let results = reactive([]);
+let result = ref();
+
+let computerPoints = ref(0);
+let playerPoints = ref(0);
 
 function setChoices(choice) {
-  playerChoice.value = choice;
-  computerChoice.value = availableChoices.value[Math.floor(Math.random() * 3)];
+  // object clone with reactivity?
+  playerChoice.name = choice.name;
+  playerChoice.icon = choice.icon;
+  let computerChoiceTemp = availableChoices[Math.floor(Math.random() * 3)];
+  computerChoice.name = computerChoiceTemp.name;
+  computerChoice.icon = computerChoiceTemp.icon;
+
+  setResult();
 }
 
-let result = computed(() => {
-  if (playerChoice.value === computerChoice.value) return "Draw";
-  if (playerChoice.value === "paper" && computerChoice.value === "rock")
-    return "Winner";
-  if (playerChoice.value === "rock" && computerChoice.value === "scissors")
-    return "Winner";
-  if (playerChoice.value === "scissors" && computerChoice.value === "paper")
-    return "Winner";
-  return "Nice try";
+function setResult() {
+  result = "Nice try";
+  if (playerChoice.name === computerChoice.name) {
+    result = "Draw";
+  }
+
+  if (
+    (playerChoice.name === "Paper" && computerChoice.name == "Rock") ||
+    (playerChoice.name === "Rock" && computerChoice.name === "Scissors") ||
+    (playerChoice.name === "Scissors" && computerChoice.name === "Paper")
+  ) {
+    playerPoints.value = playerPoints.value + 1;
+    result = "Winner";
+  }
+
+  if (result === "Nice try") {
+    computerPoints.value = computerPoints.value + 1;
+  }
+  results.push({
+    playerChoice: playerChoice.name,
+    computerChoice: computerChoice.name,
+    result: result,
+  });
+}
+
+let ratio = computed(() =>
+  Math.round((playerPoints.value / (results.length || 1)) * 100)
+);
+
+let draws = computed(() => results.filter((el) => el.result === "Draw").length);
+
+let typeOfChoices = reactive({});
+watch(results, () => {
+  typeOfChoices.paper = results.filter(
+    (el) => el.playerChoice === "Paper"
+  ).length;
+  typeOfChoices.scissors = results.filter(
+    (el) => el.playerChoice === "Scissors"
+  ).length;
+  typeOfChoices.rock = results.filter(
+    (el) => el.playerChoice === "Rock"
+  ).length;
 });
 </script>
 
 <template>
-  <div class="">
-    Computer choice:
-    <span  class="fade-in-text">{{ computerChoice.icon }} {{ computerChoice.name }}</span>
-  </div>
-  <div>
-    Player choice: <span>{{ playerChoice.icon }} {{ playerChoice.name }}</span>
-  </div>
-  <button
-    class="ma2 pointer f6 grow br-pill ph3 pv2 mb2 dib white bg-dark-green"
-    v-for="choice in availableChoices"
-    v-on:click="setChoices(choice)"
-  >
-    <span class="mr2">{{ choice.icon }}</span>{{ choice.name }}
-  </button>
-  <div>
-    Result: <span>{{ result }}</span>
-  </div>
+  <article class="center avenir mw8-ns">
+    <h1 class="f4 bg-near-white br3 br--top black-60 mv3 pv4 ph3">
+      Rock, paper, scissors
+    </h1>
+    <div class="bg-light-yellow pa3 tc">
+      Computer choice:
+      <span>{{ computerChoice.icon }} {{ computerChoice.name }}</span>
+    </div>
+    <div class="bg-light-blue pa3 tc">
+      Player choice:
+
+      <span v-if="playerChoice"
+        >{{ playerChoice.icon }} {{ playerChoice.name }}</span
+      >
+    </div>
+    <div
+      class="tc bg-animate pa4 f3"
+      :class="{
+        'bg-light-gray': result === 'Draw',
+        'bg-light-green': result === 'Winner',
+        'bg-light-red': result === 'Nice try',
+      }"
+    >
+      {{ result || "Ready" }}
+    </div>
+    <div class="pa3">
+      <button
+        class="ma2 pointer f6 grow br-pill ph3 pv2 mb2 dib white bg-dark-green"
+        v-for="choice in availableChoices"
+        v-on:click="setChoices(choice)"
+      >
+        <span class="mr2">{{ choice.icon }}</span
+        >{{ choice.name }}
+      </button>
+      <div class="bg-light-gray pa3 tc f3 lh-copy mt3">
+        <div>Computer: {{ computerPoints }} points</div>
+        <div>Player: {{ playerPoints }} points</div>
+        <div>Winn rate: {{ ratio }}%</div>
+      </div>
+
+      <div class="bg-moon-gray pa3 tc f4 lh-copy mb3" v-if="results.length">
+        <div>Nr. of choices: {{ results.length }}</div>
+        <div>Nr. of draws: {{ draws }}</div>
+        <div>
+          Player choices: <br />
+          Paper: {{ typeOfChoices.paper }}, rock: {{ typeOfChoices.rock }},
+          scissors: {{ typeOfChoices.scissors }}
+        </div>
+      </div>
+      <div class="flex flex-column flex-column-reverse">
+        <div
+          class="ph3 pv3 bb b--light-silver fade-in-text"
+          v-for="(result, index) in results"
+          :class="{
+            'bg-light-gray': result.result === 'Draw',
+            'bg-light-green': result.result === 'Winner',
+            'bg-light-red': result.result === 'Nice try',
+          }"
+        >
+          <span>
+            {{ index }}. {{ result.playerChoice }} - {{ result.result }} -
+            {{ result.computerChoice }}
+          </span>
+        </div>
+      </div>
+    </div>
+  </article>
 </template>
 
-<style scoped>
-.fade-in-text {
-  animation: fadeIn 5s;
-}
-
-@keyframes fadeIn {
-  0% { opacity: 0; }
-  100% { opacity: 1; }
-}
-</style>
+<style scoped></style>
